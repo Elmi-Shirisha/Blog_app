4bfa89fead6e80e3d4693546b21d89686650bab4
Elmineti Shirisha <elmishirisha3@gmail.com>
2024-10-25 15:09:08 +0530

    my-blogs

--- backend/index.js
+++ backend/index.js
@@ -29,18 +29,7 @@ const connectDB=async()=>{
 dotenv.config()
 app.use(express.json())
 app.use("/images",express.static(path.join(__dirname,"/images")))
-
-const allowedOrigins = ["http://localhost:5173", "https://elmi-shirisha-blogs.netlify.app/"];
-app.use(cors({
-    origin: function (origin, callback) {
-        if (allowedOrigins.includes(origin) || !origin) {
-            callback(null, true);
-        } else {
-            callback(new Error("Not allowed by CORS"));
-        }
-    },
-    credentials: true
-}));
+app.use(cors({origin:"http://localhost:5173",credentials:true}))
 app.use(cookieParser())
 app.use("/api/auth",authRoute)
 app.use("/api/users",userRoute)
